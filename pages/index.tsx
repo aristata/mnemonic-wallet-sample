4f713293d7acc88521307f7f2b4483e495aeca03
장성민 <aristatait@gmail.com>
2022-07-22 17:19:09 +0900

    Feat: 니모닉 생성시 자동으로 폼에 입력되도록 함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import useMutation from "@libs/useMutation";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import { useForm, FieldErrorsImpl } from "react-hook-form";
 
 interface MnemonicMutationResult {
@@ -18,7 +19,13 @@ interface WalletMutationResult {
   address?: string;
 }
 
+interface WalletList {
+  walletCreateForm: WalletCreateForm;
+  address: string;
+}
+
 const Home: NextPage = () => {
+  const [walletList, setWalletList] = useState<WalletList[]>([]);
   const [
     mnemonic,
     mnemonicReset,
@@ -39,20 +46,43 @@ const Home: NextPage = () => {
     register,
     handleSubmit,
     reset,
-    formState: { errors }
+    formState: { errors },
+    setValue
   } = useForm<WalletCreateForm>();
 
+  useEffect(() => {
+    if (mnemonicData) {
+      setValue("mnemonic", mnemonicData.mnemonic);
+    }
+  }, [mnemonicData]);
+
+  const [formData, setFormData] = useState<WalletCreateForm>({
+    mnemonic: "",
+    password: ""
+  });
   const onValid = (data: WalletCreateForm) => {
-    // 지갑 생성 호출
     wallet(data);
-    reset();
+    setFormData(data);
   };
+
+  useEffect(() => {
+    if (walletData?.ok) {
+      setWalletList((prev) => [
+        ...prev,
+        {
+          walletCreateForm: formData,
+          address: walletData?.address ? walletData?.address : ""
+        }
+      ]);
+    }
+  }, [formData, walletData]);
 
   const onInvalid = (errors: FieldErrorsImpl) => {
     console.error(errors);
   };
 
   const resetAll = () => {
+    reset();
     walletReset();
     mnemonicReset();
   };
@@ -72,36 +102,37 @@ const Home: NextPage = () => {
         >
           새로 만들기
         </button>
+        {mnemonicData?.ok ? (
+          <div className="py-2 space-x-2">
+            <label className="flex items-center">
+              생성된 니모닉
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 px-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </label>
+            <span>{mnemonicData.mnemonic}</span>
+          </div>
+        ) : null}
       </div>
-      {mnemonicData?.ok ? (
-        <div className="py-2 border-b-2 border-dashed space-x-2">
-          <label className="flex items-center">
-            생성된 니모닉
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 px-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 8l4 4m0 0l-4 4m4-4H3"
-              />
-            </svg>
-          </label>
-          <span>{mnemonicData.mnemonic}</span>
-        </div>
-      ) : null}
-      <div>
+
+      <div className="border-b-2 border-dashed py-2 space-x-2">
         <form
-          className="pt-4 flex flex-col space-y-2"
+          className="flex flex-col space-y-2"
           onSubmit={handleSubmit(onValid, onInvalid)}
         >
           <input
-            className="max-w-sm"
+            className="max-w-xl"
             type={"text"}
             placeholder="니모닉을 입력하세요"
             {...register("mnemonic", {
@@ -109,7 +140,7 @@ const Home: NextPage = () => {
             })}
           />
           <input
-            className="max-w-sm"
+            className="max-w-xl"
             type={"password"}
             placeholder="비밀번호를 입력하세요"
             {...register("password", {
@@ -123,50 +154,86 @@ const Home: NextPage = () => {
             지갑 생성
           </button>
         </form>
+        {!walletData ? null : walletData.ok ? (
+          <div className="py-2 border-b-2 border-dashed space-x-2">
+            <label className="flex items-center">
+              생성된 지갑
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 px-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </label>
+            <span>{walletData.address}</span>
+          </div>
+        ) : (
+          <div className="py-2 border-b-2 border-dashed space-x-2">
+            <label className="flex items-center">
+              메세지
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 px-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </label>
+            <span>{walletData?.message}</span>
+          </div>
+        )}
       </div>
-      {!walletData ? null : walletData.ok ? (
-        <div className="py-2 border-b-2 border-dashed space-x-2">
-          <label className="flex items-center">
-            생성된 지갑
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 px-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 8l4 4m0 0l-4 4m4-4H3"
-              />
-            </svg>
-          </label>
-          <span>{walletData.address}</span>
-        </div>
-      ) : (
-        <div className="py-2 border-b-2 border-dashed space-x-2">
-          <label className="flex items-center">
-            메세지
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 px-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 8l4 4m0 0l-4 4m4-4H3"
-              />
-            </svg>
-          </label>
-          <span>{walletData?.message}</span>
-        </div>
-      )}
+
+      <div className="py-2 space-x-2">
+        {walletList.length > 0 ? (
+          <div>
+            <h3>생성된 지갑 목록</h3>
+            <table className="table-auto border-collapse border border-slate-500">
+              <thead>
+                <tr>
+                  <td className="border border-slate-500">지갑 주소</td>
+                  <td className="border border-slate-500">지갑 비밀번호</td>
+                  <td className="border border-slate-500">지갑 니모닉</td>
+                </tr>
+              </thead>
+              <tbody>
+                {walletList.map((wallet, index) => (
+                  <tr key={index}>
+                    <td className="border border-slate-500">
+                      {wallet.address}
+                    </td>
+                    <td className="border border-slate-500">
+                      {wallet.walletCreateForm.password}
+                    </td>
+                    <td className="border border-slate-500">
+                      {wallet.walletCreateForm.mnemonic}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <>
+            <span>생성된 지갑이 없습니다</span>
+          </>
+        )}
+      </div>
     </div>
   );
 };
