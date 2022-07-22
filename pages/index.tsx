412a10a7b6dfec4ed02ca9e115882d90eed1749e
장성민 <aristatait@gmail.com>
2022-07-22 15:31:21 +0900

    Style: 니모닉 표시 부분 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,15 +17,31 @@ const Home: NextPage = () => {
     <div className="py-5 px-10">
       <div className="border-b-2 border-dashed py-2">
         <button
-          className="bg-red-300 rounded-md text-white font-medium text-sm px-4 py-1"
+          className="bg-red-300 rounded-3xl text-white font-bold text-3xl p-10"
           onClick={createMnemonic}
         >
           generate mnemonic
         </button>
       </div>
       {mnemonicData?.ok ? (
-        <div className="py-2 flex flex-col border-b-2 border-dashed ">
-          <label>생성된 니모닉</label>
+        <div className="py-2 border-b-2 border-dashed space-x-2">
+          <label className="flex items-center">
+            생성된 니모닉
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 px-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </label>
           <span>{mnemonicData.mnemonic}</span>
         </div>
       ) : null}
